0773ebe231c8219fabc4268ee79be23f0b670e9c
Drew <tke578@gmail.com>
2020-03-22 11:34:54 -0700

    hot fix

--- dividend/errors.py
+++ dividend/errors.py
@@ -4,4 +4,6 @@ class Non200Response(Exception):
 		super(Non200Response, self).__init__(f'Url returned a non 200 response {url}')
 
 class BadRequest(Exception):
-	def __init__(self, error_msg).__init__(f'Something bad happened {error_msg}')
+	"""Bad Requests"""
+	def __init__(self, error_msg):
+		super(BadRequest).__init__(f'Something bad happened {error_msg}')
