df41776f17d6db963618455dc21b2a3af85be1a4
Drew <tke578@gmail.com>
2020-03-24 23:55:29 -0700

    fix

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='dividend',
-	  packages=['dividend', 'dividend.parsers'],
+	    packages=['dividend', 'dividend.parsers'],
       version='1.0.0',
-      description='Unofficail API for Dividend.com',
-      url='http://github.com/storborg/funniest',
+      description='Unofficial API for Dividend.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/tke578/dividend',
       author='Andrew Garcia',
       author_email='tke578@gmail.com',
       keywords = ['dividend', 'api', 'screener', 'dividend api'],
